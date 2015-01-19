75dd2c834fea210a9458bed8b6ea9ee4d21956bb
Aaron J Trevena <aaron@aarontrevena.co.uk>
2015-01-19 09:51:47 +0000

    fix missing pre-req, bump version

--- lib/Email/Assets.pm
+++ lib/Email/Assets.pm
@@ -7,11 +7,11 @@ Email::Assets - Manage assets for Email
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 use MIME::Types;
 use Email::Assets::File;
